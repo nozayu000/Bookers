27505a91e2c92d2ddc76fb9cc882827030d47e40
nozayu0000 <n.yuntamu@gmail.com>
2021-09-19 05:53:28 +0000

    create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,43 +1,38 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new(book_path)
   end
 
   def show
      @book = Book.find(params[:id])
   end
 
-  def new
-    @book = Book.new
-  end
-
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book)
     else
       render :new
     end
   end
-  
-  def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
-  end
 
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book)
+  end
 
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
+    @book = Book.find(params[:id])
+    @book.destroy
     redirect_to books_path  
   end
-  
-  
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
