cad8e7dde5b1a2604a3756d085b5c5bdebd5635a
nozayu0000 <n.yuntamu@gmail.com>
2021-09-23 04:36:57 +0000

    add edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,45 +1,45 @@
 class BooksController < ApplicationController
 
-  def show
-    @book = Book.find(params[:id])
-
-  end
-
-  def index
-    @books = Book.all
-    @book = Book.new
-  end
+def index
+  @books = Book.all
+  @book = Book.new
+end
 
 def create
-      @book = Book.new(book_params)
+    @book = Book.new(book_params)
     if @book.save
-      redirect_to book_path(@book), notice:"successfully created book!"
+      flash[:notice] = "successfully created book!"
+      redirect_to book_path(@book.id)
     else
       @books = Book.all
-      render 'index'
+      render  :index
     end
 end
 
-  def edit
-    @book = Book.find(params[:id])
-  end
+def show
+  @book = Book.find(params[:id])
+end
 
+def edit
+  @book = Book.find(params[:id])
+end
 
 def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      # @book
-      redirect_to book_path(@book), notice:"successfully updated book!"
+      flash[:notice] = "successfully updated book!"
+      redirect_to book_path(@book)
     else
-      #updateを失敗すると編集ページへ
-      render 'edit'
+      @books=Book.all
+      render :edit
     end
 end
 
   def destroy
-  @book = Book.find(params[:id])
-  @book.destroy
-  redirect_to books_path(@book),　notice:"successfully destroyed book!"
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:notice] = "successfully destroyed book!"
+    redirect_to books_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-root :to => 'homes#top'
 resources :books
-# get 'books' =>'books#index'
-# post 'books' => 'books#create'
-# delete 'books/:id' => 'books#destroy'
-# get 'books/:id' => 'books#show', as: 'book'
-# get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-# patch 'books/:id' => 'books#update', as: 'update_book'
-# put 'books/:id' => 'books#update'
+root :to => 'homes#top'
+# root to: 'homes#top'
+
 end
